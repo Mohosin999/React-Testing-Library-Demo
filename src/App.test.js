66bbafaeb7fcbd321f1738a8d7d342e5e0ef5558
Mohosin999 <programmermohosin@gmail.com>
2023-05-31 07:33:42 +0600

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Learn Jest");
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toHaveTextContent("Learn Jest");
+  expect(linkElement).toHaveAttribute("href", "https://reactjs.org");
 });
