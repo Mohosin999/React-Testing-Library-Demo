5e5f026d1f34f14827b1eba6a9296593c19fc807
Mohosin999 <programmermohosin@gmail.com>
2023-05-31 07:47:00 +0600

    find logo

--- src/App.test.js
+++ src/App.test.js
@@ -12,8 +12,15 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn Jest");
+  // const linkElement = screen.getByText("Learn Jest");
+  const linkElement = screen.getByTestId("React-Link");
   expect(linkElement).toBeInTheDocument();
   expect(linkElement).toHaveTextContent("Learn Jest");
   expect(linkElement).toHaveAttribute("href", "https://reactjs.org");
+});
+
+test("should return react logo", () => {
+  render(<App />);
+  const image = screen.getByAltText("logo");
+  expect(image).toBeInTheDocument();
 });
