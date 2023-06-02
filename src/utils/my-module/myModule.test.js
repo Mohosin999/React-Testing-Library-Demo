abe2e744b72d0c890ef105545978259ef568fe05
Mohosin999 <programmermohosin@gmail.com>
2023-06-02 09:34:32 +0600

    mock function and module test

--- src/utils/my-module/myModule.test.js
+++ src/utils/my-module/myModule.test.js
@@ -1,1 +1,22 @@
 import { myFunction } from "./myModule";
+
+jest.mock("./myModule", () => {
+  const originalModule = jest.requireActual("./myModule");
+  return {
+    __esModule: true,
+    ...originalModule,
+    add: jest.fn((a, b) => 7),
+  };
+});
+
+describe("Mocking My Module to Test Function", () => {
+  it('should return "Big" when the sum is 10 or greater', () => {
+    const result = myFunction(10, 6);
+    expect(result).toEqual("Big");
+  });
+
+  it('should return "Small" when the sum is less than 10', () => {
+    const result = myFunction(2, 6);
+    expect(result).toEqual("Small");
+  });
+});
