c13688f441840bac39e393a3b17f8e461286c74d
Mohosin999 <programmermohosin@gmail.com>
2023-06-06 11:08:56 +0600

    test codes run correctly

---
+++ src/components/user-list/UserList.test.js
@@ -0,0 +1,59 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+import UserList from "./userList";
+
+jest.mock("axios");
+
+describe("User List", () => {
+  it("should display the list of users when the request is successfull", async () => {
+    const expectedUsers = [
+      { id: 1, name: "John Doe", email: "john@example.com" },
+      { id: 2, name: "Jane Smith", email: "jane@example.com" },
+    ];
+
+    axios.get.mockResolvedValueOnce({ data: expectedUsers });
+
+    render(<UserList />);
+
+    await waitFor(() => {
+      const userTable = screen.getByRole("table");
+
+      expectedUsers.forEach((user) => {
+        const idElement = screen.queryByText(user.id);
+        const nameElement = screen.queryByText(user.name);
+        const emailElement = screen.queryByText(user.email);
+
+        expect(idElement).toBeInTheDocument();
+        expect(nameElement).toBeInTheDocument();
+        expect(emailElement).toBeInTheDocument();
+      });
+    });
+  });
+
+  //   it("should display an error message when the request fails", async () => {
+  //     const expectedErrorMessage = "Network Error";
+
+  //     axios.get.mockRejectedValueOnce({ message: expectedErrorMessage });
+
+  //     render(<UserList />);
+
+  //     await waitFor(() => {
+  //       const errorElement = screen.queryByText(expectedErrorMessage);
+
+  //       expect(errorElement).toBeInTheDocument();
+  //     });
+  //   });
+  it("should display an error message when the request fails", async () => {
+    const expectedErrorMessage = "Error fetching user data";
+
+    axios.get.mockRejectedValueOnce({ message: expectedErrorMessage });
+
+    render(<UserList />);
+
+    await waitFor(() => {
+      const errorElement = screen.queryByText(expectedErrorMessage);
+
+      expect(errorElement).toBeInTheDocument();
+    });
+  });
+});
--- src/components/user-list/userList.jsx
+++ src/components/user-list/userList.jsx
@@ -1,48 +1,111 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const UserList = () => {
+//   const [user, setUser] = useState([]);
+//   const [error, setError] = useState("");
+
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       try {
+//         const response = await axios.get("https://api.example.com/users");
+//         setUser(response.data);
+//         setError("");
+//       } catch (error) {
+//         setError("Error fetching user data");
+//         setUser([]);
+//       }
+//     };
+
+//     fetchUser();
+//   }, []);
+
+//   return (
+//     <div>
+//       {error && <p>{error}</p>}
+//       {user && (
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>User Id</th>
+//               <th>Name</th>
+//               <th>Email</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {user.map((userData) => (
+//               <tr key={userData.id}>
+//                 <td>{userData.id}</td>
+//                 <td>{userData.name}</td>
+//                 <td>{userData.email}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default UserList;
+
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const UserList = () => {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get("https://api.example.com/users");
-        setUser(response.data);
-        setError(null);
-      } catch (error) {
+    axios
+      .get("/api/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
         setError("Error fetching user data");
-        setUser(null);
-      }
-    };
-
-    fetchUser();
+      });
   }, []);
 
   return (
     <div>
       {error && <p>{error}</p>}
-      {user && (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
+      {/* <table>
+        <thead>
+          <tr>
+            <th>User Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
             </tr>
-          </thead>
-          <tbody>
-            {user.map((userData) => (
-              <tr key={userData.id}>
-                <td>{userData.name}</td>
-                <td>{userData.email}</td>
-                <td>{userData.phone}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          ))}
+        </tbody>
+      </table> */}
+      <table>
+        <thead>
+          <tr>
+            <th>User Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
