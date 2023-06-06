bd2e374e65b7da764a8efcbeb20be06357962c76
Mohosin999 <programmermohosin@gmail.com>
2023-06-06 08:27:49 +0600

    commit

--- src/components/user-list/userList.jsx
+++ src/components/user-list/userList.jsx
@@ -1,7 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const userList = () => {
-  return <div></div>;
+const UserList = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("https://api.example.com/users");
+        setUser(response.data);
+        setError(null);
+      } catch (error) {
+        setError("Error fetching user data");
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {user && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.map((userData) => (
+              <tr key={userData.id}>
+                <td>{userData.name}</td>
+                <td>{userData.email}</td>
+                <td>{userData.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
 
-export default userList;
+export default UserList;
