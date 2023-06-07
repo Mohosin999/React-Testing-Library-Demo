e8fe1957ad77b4e275268b605bb2d499b9b62b91
Mohosin999 <programmermohosin@gmail.com>
2023-06-07 09:09:20 +0600

    changes some of codes

--- src/components/user-list-spinner/UserListSpinner.jsx
+++ src/components/user-list-spinner/UserListSpinner.jsx
@@ -1,3 +1,46 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+// const UserListSpinner = () => {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     axios
+//       .get("/api/users")
+//       .then((response) => {
+//         setData(response.data);
+//       })
+//       .catch((error) => {
+//         setError(error);
+//       })
+//       .finally(() => {
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       {loading && <p>Spinner Loading</p>}
+//       {error && <p>{error}</p>}
+//       {data && (
+//         <ul>
+//           {data.map((user) => (
+//             <li key={user.id}>
+//               <div>User Id: {user.id}</div>
+//               <div>Name: {user.name}</div>
+//               <div>Email: {user.email}</div>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default UserListSpinner;
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -7,23 +50,24 @@ const UserListSpinner = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("/api/users")
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/users");
         setData(response.data);
-      })
-      .catch((error) => {
+      } catch (error) {
         setError(error);
-      })
-      .finally(() => {
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <div>
-      {loading && <p>Spinner Loading...</p>}
-      {error && <p>{error}</p>}
+      {loading && <p>Spinner Loading</p>}
+      {error && <p>{error.message}</p>}
       {data && (
         <ul>
           {data.map((user) => (
--- src/components/user-list-spinner/UserListSpinner.test.js
+++ src/components/user-list-spinner/UserListSpinner.test.js
@@ -9,9 +9,59 @@ describe("User List Spinner", () => {
     jest.resetAllMocks();
   });
 
-  it("should display a loading spinner while the request is in progress", async () => {});
+  it("should display a loading spinner while the request is in progress", () => {
+    render(<UserListSpinner />);
+    const loadingSpinner = screen.getByText("Spinner Loading");
+    expect(loadingSpinner).toBeInTheDocument();
+  });
 
-  it("should display a list of user successfull", async () => {});
+  //   it("should display a list of users successfully", async () => {
+  //     const mockData = [
+  //       { id: 1, name: "John Doe", email: "john.doe@example.com" },
+  //       { id: 2, name: "Jane Smith", email: "jane.smith@example.com" },
+  //     ];
+  //     axios.get.mockResolvedValue({ data: mockData });
 
-  it("should display an error message when the request face", async () => {});
+  //     render(<UserListSpinner />);
+
+  //     await waitFor(() => {
+  //       const userElements = screen.getAllByRole("listitem");
+  //       expect(userElements).toHaveLength(2);
+  //     });
+  //   });
+
+  //   it("should display an error message when the request fails", async () => {
+  //     const errorMessage = "Request failed";
+  //     axios.get.mockRejectedValue(new Error(errorMessage));
+
+  //     render(<UserListSpinner />);
+
+  //     await waitFor(() => {
+  //       const errorElement = screen.getByText(errorMessage);
+  //       expect(errorElement).toBeInTheDocument();
+  //     });
+  //   });
 });
+
+// import { render, screen, waitFor } from "@testing-library/react";
+// import axios from "axios";
+// import UserListSpinner from "./UserListSpinner";
+
+// jest.mock("axios");
+
+// describe("User List Spinner", () => {
+//   afterEach(() => {
+//     jest.resetAllMocks();
+//   });
+
+//   it("should display a loading spinner while the request is in progress", () => {
+//     render(<UserListSpinner />);
+
+//     const loadingSpinner = screen.getByText("Spinner Loading");
+//     expect(loadingSpinner).toBeInTheDocument();
+//   });
+
+//   it("should display a list of user successfull", async () => {});
+
+//   it("should display an error message when the request face", async () => {});
+// });
